299423beb730ef45ab97e4d3d5bc38c76dcfb138
David Gillis <dgillis91@gmail.com>
2019-09-20 06:54:59 -0500

    remove debug lines

--- src/main.c
+++ src/main.c
@@ -7,9 +7,5 @@
 int main(int argc, char* argv[]) {
     parse_options(argc, argv);
 
-    printf("Allowable Run Time: %d\n", get_allowable_run_time());
-    printf("Log Path: %s\n", get_logfile_path());
-    printf("Max Children: %d\n", get_max_child_process_count());
-
     return EXIT_SUCCESS;    
 }
