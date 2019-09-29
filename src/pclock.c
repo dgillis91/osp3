28b828c5bff9179b407aa4c6ba4742856610bfdd
David Gillis <dgillis91@gmail.com>
2019-09-29 11:30:05 -0500

    added support for sync access to system_clock

--- src/pclock.c
+++ src/pclock.c
@@ -1,4 +1,6 @@
 #include <signal.h>
+#include <stdio.h>
+#include <errno.h>
 #include <sys/sem.h>
 #include <sys/shm.h>
 #include <sys/stat.h>
@@ -7,15 +9,52 @@
 #include "../include/shmutil.h"
 #include "../include/semutil.h"
 
+#define PERM (S_IRUSR | S_IWUSR)
+
 
 static int semid;
 static struct sembuf semlock;
-static struct sembuf sumunlock;
+static struct sembuf semunlock;
 static pclock_t* system_clock;
 
 
 int init_clock(int key, int is_child) {
-    // TODO
+    int shid;
+
+    setsembuf(&semlock, 0, -1, 0);
+    setsembuf(&semunlock, 0, 1, 0);
+
+    shid = shmget(key, sizeof(pclock_t), PERM | IPC_CREAT | IPC_EXCL);
+
+    // Couldn't gedt shared memory, and there's a real error.
+    if ((shid == -1) && errno != EEXIST) {
+        return -1;
+    }
+
+    // Already created. Attach. 
+    if (shid == -1) {
+        // Access
+        if ((shid = shmget(key, sizeof(pclock_t), PERM)) == -1) {
+            return -1;
+        }
+        // Attach
+        if ((system_clock = (pclock_t*) shmat(shid, NULL, 0)) == (void*)(-1)) {
+            return -1;
+        }
+    } else {
+        // Create in the first shmget call. 
+        system_clock = (pclock_t*) shmat(shid, NULL, 0);
+        if (system_clock == (void*)(-1)) {
+            return -1;
+        }
+        system_clock->nanoseconds = 0;
+        system_clock->total_tick = 0;
+        system_clock->seconds = 0;
+    }
+    semid = initsemset(key, 1, &system_clock->ready);
+    if (semid == -1) {
+        return -1;
+    }
     return 0;
 }
 
@@ -28,10 +67,16 @@ pclock_t get_copy() {
 }
 
 
-void tick(int nanoseconds) {
-    /* Increment the system clock by `nanoseconds`
+int tick(int nanoseconds) {
+    /* Increment the system clock by `nanoseconds`.
+    ** Method synchronizes access to `system_clock`.
     */
+    if (semop(semid, &semlock, 1) == -1)
+        return -1;
     add_in_place(system_clock, nanoseconds);
+    if (semop(semid, &semunlock, 1) == -1) 
+        return -1;
+    return 0;
 }
 
 
@@ -42,6 +87,10 @@ pclock_t add(pclock_t clock, unsigned int nanoseconds) {
 
 
 void add_in_place(pclock_t* clock, unsigned int nanoseconds) {
+    /* Add `nanoseconds` to `clock`. Addition done in place.
+    ** No regard given to synchronization. Methods acting on
+    ** `clock` should work with `clock->readyp` externally.
+    */
     clock->total_tick += nanoseconds;
     clock->nanoseconds = clock->total_tick % NANO_SEC_IN_SEC;
     clock->seconds = clock->total_tick / NANO_SEC_IN_SEC;
@@ -49,5 +98,10 @@ void add_in_place(pclock_t* clock, unsigned int nanoseconds) {
 
 
 int is_equal_to_sys_clock(pclock_t clock) {
-    return clock.total_tick == system_clock->total_tick;
+    if (semop(semid, &semlock, 1) == -1) 
+        return -1;
+    int is_equal = clock.total_tick == system_clock->total_tick;
+    if (semop(semid, &semunlock, 1) == -1)
+        return -1;
+    return is_equal;
 }
