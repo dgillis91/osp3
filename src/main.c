31d506cd16b30def89d8bf62c995f8ac07ce2c09
David Gillis <dgillis91@gmail.com>
2019-09-30 18:09:10 -0500

    implement child exec

--- src/main.c
+++ src/main.c
@@ -1,22 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <semaphore.h>
-#include "../include/semutil.h"
+
 #include "../include/pclock.h"
 #include "../include/parse.h"
-
-
-#include "../include/parse.h"
+#include "../include/util.h"
 
 
 #define CHILD_PROCESS 0
+#define KEY 8675309
 
 
 int main(int argc, char* argv[]) {
     parse_options(argc, argv);
     
     unsigned int max_child_process_count = get_max_child_process_count();
+
+
+    // Initialize the system clock
+    int clock_shid;
+    if ((clock_shid = init_clock(KEY)) == -1) {
+        perror("Failed to initialize system clock.");
+        return EXIT_FAILURE;
+    }
 
     pid_t* child_process_handle = NULL;
     if ((child_process_handle = (pid_t*) malloc(sizeof(pid_t) * max_child_process_count)) == NULL) {
@@ -42,15 +48,22 @@ int main(int argc, char* argv[]) {
     // Loop and check shared memory 
     // TODO: Move child pid termination checking code
     // and semaphors to a sep file. 
-    while (1) {
-        
-    }
 
     // Wait for all child processes to terminate.
     if (spawned_proc_id) {
+        while (get_nano() < 100) {
+            tick(1);
+            fprintf(stderr, "System Time %d\n", get_nano());
+        }
         int status = 0;
         pid_t wait_pid;
-        while((wait_pid = wait(&status)) > 0) ;
+        while((wait_pid = wait(&status)) > 0);
+        if (destruct_clock(KEY, clock_shid) == -1) {
+            perror("Failed to deallocate clock");
+        }
+    } else {
+        execl("user", "user", NULL);
+        //fprintf(stderr, "[%ld] System Time: %d\n", (long) getpid(), get_nano());
     }
 
     return EXIT_SUCCESS;    
