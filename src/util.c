f19422c70a5fcc207a0277709e0eec525a50e001
David Gillis <dgillis91@gmail.com>
2019-09-30 06:19:33 -0500

    initial commit

---
+++ src/util.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+
+#include "../include/util.h"
+
+
+unsigned int rand_between(unsigned int lower, unsigned int upper) {
+    /* Return a random number between `lower` and `upper`.
+    ** Note that we don't seed. Additionally, we are working with 
+    ** the naturals mod upper.
+    */
+    return (rand() % (upper - lower + 1)) + lower;
+}
