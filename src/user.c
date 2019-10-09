ada7ba19940d993da7abfcc4611bc2eb15e1650e
David Gillis <dgillis91@gmail.com>
2019-10-08 22:29:29 -0500

    comment

--- src/user.c
+++ src/user.c
@@ -23,11 +23,13 @@ int main(int argc, char* argv[]) {
     int system_clock_shid = init_clock(CLOCK_KEY);
     int proc_shid = init_proc_handle(PROC_KEY);
 
+    // Compute stop time.
     pclock_t stop_time;
     pclock_t local_clock;
     get_copy(&local_clock);
     stop_time = add(local_clock, run_time);
 
+    // Get busy - check your clock.
     while (1) {
         if (get_total_tick() >= stop_time.total_tick || get_is_abrupt_terminate()) {
             mark_ready_to_terminate();
